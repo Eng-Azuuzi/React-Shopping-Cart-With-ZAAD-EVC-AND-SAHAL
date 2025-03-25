02f16e6a9258c99f3e79ddce2353714a3974a470
Eng-Azuuzi <apdiasiisipraahim118@gmail.com>
2025-03-25 09:46:48 +0300

    updated import index/css to Index/css

--- src/Index.jsx
+++ src/Index.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ShopProvider } from "./ShopContext";
-import "./index.css";
+import "./Index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
